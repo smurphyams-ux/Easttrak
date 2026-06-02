023a82c6fb0faa0c8488ae3f7bf82c43a50d5b19
Sandra Murphy <sdm6160@gmail.com>
2026-06-02 17:03:26 -0400

    Make Render build Linux-safe and remove fragile route import

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import * as RoutesPageModule from './pages/RoutesPage';
 import DashboardScreen from './screens/DashboardScreen';
 import InvoicesPanel from './screens/InvoicesPanel';
-
-const RoutesPage =
-  (RoutesPageModule as any).default ||
-  (RoutesPageModule as any).RoutesPage ||
-  (() => null);
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/dashboard" element={<DashboardScreen />} />
-        <Route path="/route" element={<RoutesPage />} />
+        <Route path="/route" element={<DashboardScreen />} />
         <Route path="/invoices" element={<InvoicesPanel />} />
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
